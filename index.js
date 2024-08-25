a459f95800bb4b849ab5e01ba6f179dc19df666e
Kevin <kramzyvine@icloud.com>
2024-08-25 22:36:47 +0300

    Completed project

--- index.js
+++ index.js
@@ -1,127 +1,93 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-// configurations
-process.env.MONGO_URI="mongodb+srv://kevobizzi:ramogi4960@cluster0.uvbbx.mongodb.net/Exercise?retryWrites=true&w=majority&appName=Cluster0";
-
-
-// cloud database connection
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect('mongodb+srv://kevobizzi:ramogi4960@cluster0.uvbbx.mongodb.net/Exer?retryWrites=true&w=majority&appName=Cluster0', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
-// design the database and its queries
-let Person = mongoose.model("Exercises", mongoose.Schema({
+let UserExercise = mongoose.model("UserExercise", mongoose.Schema({
   username: String,
   count: Number,
   log: [{
     description: String,
     duration: Number,
-    date: Number,
-  }],
+    date: String,
+  }]
 }));
 
-// create new username
-// const newUsername = (name, next) => {
-//   let document = Person({
-//     username: name,
-//     count: 0,
-//     log: [{
-//       description: "not yet",
-//       duration: 0,
-//       date: 0
-//     }],
-//   });
-// };
-
-
-// end of database design
-
-app.use(cors())
-app.use(express.static('public'))
+app.use(cors());
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// homepage
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-// handle new username post requests
-app.post("/api/users", (req, res) => {
-  let document = Person({
+app.post('/api/users', async (req, res) => {
+  let document = await UserExercise({
     username: req.body.username,
     count: 0,
-    log: [{
-      description: "not yet",
-      duration: 0,
-      date: 0
-    }],
-  });
-  document.save();
-  console.log(document);
+    log: []
+  }).save();
+
   res.json({
-    username: document.username,
     _id: document._id,
+    username: document.username,
   });
 });
 
-app.get("/api/users", (req, res) => {
-  Person.find({}).select("username").then(data => {
-    res.json(data);
+app.get('/api/users', async (req, res) => {
+  let all_documents = await UserExercise.find({}).select("username _id");
+  res.json(all_documents);
+});
+
+app.post("/api/users/:_id/exercises", async (req, res) => {
+  // first get the log items from the request object
+  const { description } = req.body;
+  const duration = +req.body.duration;
+  let date = new Date(req.body.date).toDateString() === "Invalid Date" ? new Date().toDateString() : new Date(req.body.date).toDateString();
+  let document = await UserExercise.findByIdAndUpdate(req.params._id, {
+    $inc: {
+      count: 1
+    },
+    $push: {
+      log: { description, duration, date }
+    }
+  }, { new: true });
+  res.json({
+    _id: document._id,
+    username: document.username,
+    description, duration, date
   });
 });
 
-// testing _id = 66c66a36e1d17c38dbb40a75
-app.post("/api/users/:_id/exercises", (req, res) => {
-  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-
-  Person.findById(req.params._id).then(data => {
-    let { username, count, log } = data;
-    log[count].description = req.body.description;
-    log[count].duration = +req.body.duration;
-    log[count].date = new Date(req.body.date).getTime() || new Date();
-    log.push({
-      description: 'not yet',
-      duration: 0,
-      date: 0,
-    });
-    count++;
-    Person.findByIdAndUpdate(req.params._id, {
-      log: log,
-      count: count,
-    }).then(data2 => {
-      let temp = new Date(req.body.date) || new Date();
-      res.json({
-        _id: req.params._id,
-        username,
-        description: req.body.description,
-        duration: +req.body.duration,
-        date: `${days[temp.getDay()]} ${months[temp.getMonth()]} ${temp.getDate() < 10 ? "0" + temp.getDate() : temp.getDate()} ${temp.getFullYear()}`,
-      });
-    });
+app.get("/api/users/:_id/logs", async (req, res) => {
+  let document = await UserExercise.findById(req.params._id);
+  let { log, count } = document;
+  let { from, to, limit } = req.query;
+  from = from ? new Date(from).getTime() : 0;
+  to = to ? new Date(to).getTime() : new Date().getTime();
+  limit = limit ? +limit : count;
+  log = log.map(item => ({
+    description: item.description,
+    duration: item.duration,
+    date: item.date,
+  })).filter(thing => from < new Date(thing.date).getTime() < to).slice(0, limit);
+  res.json({
+    count: limit,
+    log
   });
+
 });
 
-app.get("/api/users/:_id/logs", (req, res) => {
-  Person.findById(req.params._id).select("count log -_id").then(data => {
-    res.json({
-      count: data.count,
-      log: data.log.map((item) => { 
-        return {
-          description: item.description,
-          duration: item.duration,
-          date: item.date,
-        };
-       }),
-    });
-  });
-});
+
+
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
